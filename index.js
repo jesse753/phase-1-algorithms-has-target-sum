8afbd7934ea70134d7debba19233ba260f61459f
jesse <jesse.kibaara@student.moringaschool.com>
2023-01-16 12:35:47 +0300

    done lab

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+  const seen = new Set();
+  for (const num of array) {
+    if (seen.has(target - num)) {
+      return true;
+    }
+    seen.add(num);
+  }
+  return false;
 }
+
+
+
 
 /* 
   Write the Big O time complexity of your function here
-*/
+*/O(n);
 
 /* 
   Add your pseudocode here
@@ -12,7 +22,9 @@ function hasTargetSum(array, target) {
 
 /*
   Add written explanation of your solution here
-*/
+*/the function iterates through the input array using a for-of loop. For each number in the array, it checks if the target minus that number is in the set seen. If it is, it means that we have found a pair of numbers that add up to the target and the function returns true. If the number is not in the set, it adds it to the set.
+
+After iterating through the entire array, if no pair of numbers adding up to the target has been found, the function returns false.
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
